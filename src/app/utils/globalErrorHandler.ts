6dd09e78b71b8601166d702b72584c96f123e141
alamin <alaminsf40@gmail.com>
2023-12-02 14:54:28 +0600

    fix

--- src/app/utils/globalErrorHandler.ts
+++ src/app/utils/globalErrorHandler.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from 'express';
 
 const globalErrorHandler = (
-  err,
+  err: any,
   req: Request,
   res: Response,
   next: NextFunction,
--- src/app/utils/notFoundErrorHndler.ts
+++ src/app/utils/notFoundErrorHndler.ts
@@ -1,6 +1,8 @@
-import { RequestHandler } from 'express';
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, RequestHandler } from 'express';
 
-const notFoundErrorHandler: RequestHandler = (req, res) => {
+const notFoundErrorHandler: RequestHandler = (req, res, next: NextFunction) => {
   res.status(200).json({
     success: false,
     message: 'Api Not Found',
